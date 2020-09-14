ed6b02cdcd383e03eb7c7864f3e67569f27eeb67
Ankit <ankitsisodiya76@gmail.com>
2020-09-15 02:45:01 +0530

    pet cards

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+           $('#loginTrigger').click(function(){
+$('#loginModal').modal();
+           });
+           $('#registerTrigger').click(function(){
+$('#registerModal').modal();
+           });
+        });
