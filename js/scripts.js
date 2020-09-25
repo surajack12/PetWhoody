7f0f36fbe5504b87c08e02621a7c12f018fa2e45
Ankit <ankitsisodiya76@gmail.com>
2020-09-26 00:55:34 +0530

    adoption fee form

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,38 @@ $('#loginModal').modal();
            $('#registerTrigger').click(function(){
 $('#registerModal').modal();
            });
+           $('#btn-free').click(function(){
+            
+            replaceClass("btn-free","visible","invisible");
+            replaceClass("btn-notfree","invisible","visible");
+             });
+           $('#btn-notfree').click(function(){
+            replaceClass("btn-notfree","visible","invisible");
+            replaceClass("btn-free","invisible","visible");
+           });
         });
-$(document).on("click", "i.del" , function() {
-	$(this).parent().remove();
-});
+function replaceClass(id, oldClass, newClass) {
+  var elem =document.getElementById(id);
+  
+  if ($(elem).hasClass(oldClass)) {
+    $(elem).removeClass(oldClass);
+    
+  }
+  $(elem).addClass(newClass);
+}
+
 $(function() {
     $(document).on("change",".uploadFile", function()
     {
     		var uploadFile = $(this);
         var files = !!this.files ? this.files : [];
-        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+        if (!files.length || !window.FileReader) return;
  
-        if (/^image/.test( files[0].type)){ // only image file
-            var reader = new FileReader(); // instance of the FileReader
-            reader.readAsDataURL(files[0]); // read the local file
+        if (/^image/.test( files[0].type)){
+            var reader = new FileReader(); 
+            reader.readAsDataURL(files[0]);
  
-            reader.onloadend = function(){ // set image data as background of div
-                //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+            reader.onloadend = function(){ 
 uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url("+this.result+")");
             }
         }
