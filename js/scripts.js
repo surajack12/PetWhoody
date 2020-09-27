ad96ab3e835ace71e8c3d78f93a0c794dabb5539
Ankit <ankitsisodiya76@gmail.com>
2020-09-27 16:19:45 +0530

    filter for free

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,35 @@ $('#loginModal').modal();
            });
            $('#registerTrigger').click(function(){
 $('#registerModal').modal();
-           });
+           });     
+
 $("#petsearch").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
+     var value = $(this).val().toLowerCase();
     $("#petgrid .pet-col").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });           
         });
+function filterfree(chkfree) {
+        if(chkfree.checked) 
+        {
+           var value = "free";
+          $("#petgrid .pet-col").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+        }
+        else
+        {
+          $("#petgrid .pet-col").show();
+        }
+    }
  function disableadoptionfee(checkfree) {
         var petfee = document.getElementById("pet-fee");
         petfee.value=0;
         petfee.disabled = !checkfree.checked ? false : true;
         if (!petfee.disabled) {
             petfee.focus();
+            petfee.value=null;
         }
     }
 function replaceClass(id, oldClass, newClass) {
